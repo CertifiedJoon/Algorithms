032bd86ee792136ea44944f922ece7985ff7de49
CertifiedJoon <u3566665@connect.hku.hk>
2021-10-26 14:00:44 +0000

    Adaptable queue class implemented

---
+++ queue_algorithms/AdaptableHeapPriorityQueue.py
@@ -0,0 +1,17 @@
+from ./HeapPriorityQueue import HeapPriorityQueue
+
+class AdaptableHeapPriorityQueue(HeapPriorityQueue):
+    class Locator(HeapPriorityQueue._Item):
+        __slots__ =  '_index'
+        def __init__(self, pos, key, val):
+            super().__init__(key, val)
+            self._index = pos
+    def _swap(self, i, j):
+        super()._swap(i, j)
+        self._data[i]._index = j
+        self._data[j]._index = i
+    def _bubble(self, j):
+        if j > 0 and self._data[j] < self._data[self._parent(j)]:
+            self._upheap(j)
+        else:
+            self._downheap(j)
